7d994ef31275cb6e56a392a7c716004f4f46126d
Kenneth Matthew Yonathan <kennethmatthewyonathan@Kenneths-MacBook-Air.local>
2026-04-26 10:12:41 +0700

    Fix util.py

--- util.py
+++ util.py
@@ -56,10 +56,14 @@ class Tester:
         datapoint = self.data[i]
         value = self.predictor(datapoint)
         guess = self.post_process(value)
-        truth = datapoint.get("Future Price") or float(datapoint["completion"])
+        if "Future Price" in datapoint:
+            truth = datapoint["Future Price"]
+            title = datapoint["Ticker"]
+        else:
+            truth = float(datapoint["completion"])
+            title = datapoint["prompt"]
         error = abs(guess - truth)
         color = self.color_for(error, truth)
-        title = datapoint.get["Ticker"] or datapoint["prompt"]
         return title, guess, truth, error, color
 
     def chart(self, title):
